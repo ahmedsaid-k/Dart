e438a41908d744b718e58a9af292d1e4b645114f
ahmed said <40060021-ahmedsaidkhalil@users.noreply.replit.com>
2025-02-27 18:36:59 +0000

    One App for Dart <calculator app verion 1.0.01>

--- main.dart
+++ main.dart
@@ -1,9 +1,80 @@
-import 'dart:io';
-import 'test.dart';
+import "dart:io";
 
 void main() {
-  int sum = add(5, 5);
-  print(sum);
-  double sum2 = div(5, 1);
-  print(sum2);
+  print("Enter your name: ");
+  var name = stdin.readLineSync();
+  myApp(name);
+}
+
+void myApp(name) {
+  print('Welcome $name to calculetor');
+  int rundom = 0; //***عشان احسب عدد العمليات اللي تمت***
+  while (true) {
+    print('choce number: ');
+    print('1-oper (+).');
+    print('2-oper (-).');
+    print('3-oper (*).');
+    print('4-oper (/).');
+    print('5-exit.');
+
+    int opr = int.parse(stdin.readLineSync()!);
+    if (opr == 5) {
+      print('done.');
+      break;
+    } else if (opr != 1 && opr != 2 && opr != 3 && opr != 4 && opr != 5) {
+      print('⚠️erorr, try agan. ⚠️');
+      continue;
+    }
+
+    switch (opr) {
+      case 1:
+        print("Enter one number: ");
+        double num1 = double.parse(stdin.readLineSync()!);
+        print("Enter two number: ");
+        double num2 = double.parse(stdin.readLineSync()!);
+        double res = num1 + num2;
+        print("➡️Result: $num1 + $num2 = $res");
+        rundom++;
+        print(rundom);
+        break;
+
+      case 2:
+        print("Enter one number: ");
+        double num1 = double.parse(stdin.readLineSync()!);
+        print("Enter two number: ");
+        double num2 = double.parse(stdin.readLineSync()!);
+        double res = num1 - num2;
+        print("➡️Result: $num1 - $num2 = $res");
+        rundom++;
+        print(rundom);
+        break;
+      case 3:
+        print("Enter one number: ");
+        double num1 = double.parse(stdin.readLineSync()!);
+        print("Enter two number: ");
+        double num2 = double.parse(stdin.readLineSync()!);
+        double res = num1 * num2;
+        print("➡️Result: $num1 * $num2 = $res");
+        rundom++;
+        print(rundom);
+        break;
+      case 4:
+        print("Enter one number: ");
+        double num1 = double.parse(stdin.readLineSync()!);
+        print("Enter two number: ");
+        double num2 = double.parse(stdin.readLineSync()!);
+        if (num2 == 0) {
+          print("⚠️غير مسموح بالقسمه علي رق صفر!⚠️ ");
+        } else {
+          double res = num1 / num2;
+          print("➡️Result: $num1 / $num2 = $res");
+          rundom++;
+          print(rundom);
+        }
+
+        break;
+      default:
+        print("⚠️erorr.⚠️");
+    }
+  }
 }
--- test.dart
+++ test.dart
@@ -1,7 +1,1 @@
-int add(int x, int y) {
-  return x + y;
-}
 
-double div(int z, int c) {
-  return z / c;
-}
