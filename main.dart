3a1d5b8d781797869274b05814b11fdf030f2d86
ahmed said <40060021-ahmedsaidkhalil@users.noreply.replit.com>
2025-02-28 06:08:53 +0000

    Update App verion 1.00.02

--- main.dart
+++ main.dart
@@ -1,80 +1,82 @@
 import "dart:io";
 
 void main() {
-  print("Enter your name: ");
-  var name = stdin.readLineSync();
-  myApp(name);
+  myApp();
 }
 
-void myApp(name) {
-  print('Welcome $name to calculetor');
-  int rundom = 0; //***عشان احسب عدد العمليات اللي تمت***
+void myApp() {
+  print('Welcome to calculator');
+  int numberofoperations = 0; //***عشان احسب عدد العمليات اللي تمت***
   while (true) {
-    print('choce number: ');
-    print('1-oper (+).');
-    print('2-oper (-).');
-    print('3-oper (*).');
-    print('4-oper (/).');
-    print('5-exit.');
+    print('choice number: ');
+    print('1-operation (+).');
+    print('2-operation (-).');
+    print('3-operation (*).');
+    print('4-operation (/).');
+    print('5-Exit.');
 
-    int opr = int.parse(stdin.readLineSync()!);
-    if (opr == 5) {
-      print('done.');
+    int operation = int.parse(stdin.readLineSync()!);
+    if (operation == 5) {
+      print('Thank you for using the calculator.');
       break;
-    } else if (opr != 1 && opr != 2 && opr != 3 && opr != 4 && opr != 5) {
-      print('⚠️erorr, try agan. ⚠️');
+    } else if (operation != 1 &&
+        operation != 2 &&
+        operation != 3 &&
+        operation != 4 &&
+        operation != 5) {
+      print('⚠️Erorr, try agan. ⚠️');
       continue;
     }
 
-    switch (opr) {
+    switch (operation) {
       case 1:
-        print("Enter one number: ");
-        double num1 = double.parse(stdin.readLineSync()!);
-        print("Enter two number: ");
-        double num2 = double.parse(stdin.readLineSync()!);
-        double res = num1 + num2;
-        print("➡️Result: $num1 + $num2 = $res");
-        rundom++;
-        print(rundom);
+        print("Enter the first number: ");
+        double first_number = double.parse(stdin.readLineSync()!);
+        print("Enter the second number: ");
+        double second_number = double.parse(stdin.readLineSync()!);
+        double result = first_number + second_number;
+        print("➡️Result: $first_number + $second_number = $result");
+        numberofoperations++;
+        print("عدد العمليات التي تمت : $numberofoperations");
         break;
 
       case 2:
-        print("Enter one number: ");
-        double num1 = double.parse(stdin.readLineSync()!);
-        print("Enter two number: ");
-        double num2 = double.parse(stdin.readLineSync()!);
-        double res = num1 - num2;
-        print("➡️Result: $num1 - $num2 = $res");
-        rundom++;
-        print(rundom);
+        print("Enter the first number: ");
+        double first_number = double.parse(stdin.readLineSync()!);
+        print("Enter the second number: ");
+        double second_number = double.parse(stdin.readLineSync()!);
+        double result = first_number - second_number;
+        print("➡️Result: $first_number - $second_number = $result");
+        numberofoperations++;
+        print("عدد العمليات التي تمت : $numberofoperations");
         break;
       case 3:
-        print("Enter one number: ");
-        double num1 = double.parse(stdin.readLineSync()!);
-        print("Enter two number: ");
-        double num2 = double.parse(stdin.readLineSync()!);
-        double res = num1 * num2;
-        print("➡️Result: $num1 * $num2 = $res");
-        rundom++;
-        print(rundom);
+        print("Enter the first number: ");
+        double first_number = double.parse(stdin.readLineSync()!);
+        print("Enter the second number: ");
+        double second_number = double.parse(stdin.readLineSync()!);
+        double result = first_number * second_number;
+        print("Result: $first_number * $second_number = $result");
+        numberofoperations++;
+        print("عدد العمليات التي تمت : $numberofoperations");
         break;
       case 4:
-        print("Enter one number: ");
-        double num1 = double.parse(stdin.readLineSync()!);
-        print("Enter two number: ");
-        double num2 = double.parse(stdin.readLineSync()!);
-        if (num2 == 0) {
-          print("⚠️غير مسموح بالقسمه علي رق صفر!⚠️ ");
+        print("Enter the first number: ");
+        double first_number = double.parse(stdin.readLineSync()!);
+        print("Enter the second number: ");
+        double second_number = double.parse(stdin.readLineSync()!);
+        if (second_number == 0) {
+          print("⚠️غير مسموح بالقسمه علي رقم صفر!⚠️ ");
         } else {
-          double res = num1 / num2;
-          print("➡️Result: $num1 / $num2 = $res");
-          rundom++;
-          print(rundom);
+          double result = first_number / second_number;
+          print("➡️Result: $first_number / $second_number = $result");
+          numberofoperations++;
+          print("عدد العمليات التي تمت : $numberofoperations");
         }
 
         break;
       default:
-        print("⚠️erorr.⚠️");
+        print("⚠️Erorr.⚠️");
     }
   }
 }
